e0770363bb2ca7bae6e06700fbd6126392e6fd7d
Blanquart <alexis.blanquart@epsi.fr>
2020-11-13 17:56:41 +0100

    recup les datas de la bdd

--- src/Controller/SoireeController.php
+++ src/Controller/SoireeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Personne;
 
 class SoireeController extends AbstractController
 {
@@ -20,7 +21,16 @@ class SoireeController extends AbstractController
     /**
     * @Route("/", name="home")
     */
-    public function home() {
-    	return $this->render("soiree/home.html.twig");
+    public function home()
+    {
+
+        $repo = $this->getDoctrine()->getRepository(Personne::class);
+
+        $personnes = $repo->findAll();
+
+    	return $this->render('soiree/home.html.twig', [
+    	    'controller_name' => 'SoireeController',
+            'personnes' => $personnes
+        ]);
     }
 }
