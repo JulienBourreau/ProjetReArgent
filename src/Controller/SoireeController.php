a0298b0a9b6ad0297aca1119a2ab146229f4a0c7
Blanquart <alexis.blanquart@epsi.fr>
2020-11-13 20:32:00 +0100

    Il faut corriger "ajouter personne" faire un modif et un suppr

--- src/Controller/SoireeController.php
+++ src/Controller/SoireeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\AjoutPersonneType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,6 +32,32 @@ class SoireeController extends AbstractController
     	return $this->render('soiree/home.html.twig', [
     	    'controller_name' => 'SoireeController',
             'personnes' => $personnes
+        ]);
+    }
+
+    /**
+     * @Route("/soiree",name="personne_ajouter")
+     */
+    public function ajouter(Request $request)
+    {
+        $personne = new Personne();
+
+        $form = $this->createForm(AjoutPersonneType::class, $personne);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //récupération de l'entityManager
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($personne);
+
+            $em->flush();
+
+            return $this->redirectToRoute("personne", ["idPersonne"=>$personne->getNom()->getId()]);
+        }
+
+        return $this->render("soiree/index.html.twig", [
+            "formulaire" => $form->createView()
         ]);
     }
 }
